ba3bffb47731415010daa35fd618af6209df483f
Conner Leblanc <53800691+connerleb@users.noreply.github.com>
2025-07-10 15:42:46 -0600

    Changed db to different env variables in order to host for free online

--- Backend/server.js
+++ Backend/server.js
@@ -18,11 +18,11 @@ app.use(cors());
 
 // create the connection to the database
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    port: process.env.MYSQL_ADDON_PORT || 3306,
 });
 
 // connect to the database
