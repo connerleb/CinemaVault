aa876b8d7cd2ae351febc4b16415eb2dbcde1851
Conner Leblanc <53800691+connerleb@users.noreply.github.com>
2025-07-11 15:54:44 -0600

    Updated the profile page to display more options

--- Frontend/src/pages/profile.jsx
+++ Frontend/src/pages/profile.jsx
@@ -1,8 +1,11 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 
-function profile() {
+
+function Profile({setUser}) {
     const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [accountInfo, setAccountInfo] = useState("");
 
     // when the page is loaded check if the user is logged in
     useEffect(() => {
@@ -11,11 +14,51 @@ function profile() {
             navigate('/login');
         }
     }, []);
+
+    useEffect(() => {
+        async function getUserInfo(){
+            const user = localStorage.getItem('username');
+            const response = await fetch(`/userinfo/${user}`);
+            if (!response.ok) {
+                return console.error(response);
+            }
+
+            const data = await response.json();
+            setUsername(data.username);
+            setAccountInfo(data);
+        }
+        getUserInfo();
+    }, []);
+
+
+
+    const logout = () => {
+        localStorage.removeItem('username');
+        setUser(null);
+        navigate('/login');
+    }
+
+    const userNav = () => {
+        navigate('/changeUsername');
+    }
+
+    const passNav = () => {
+        navigate('/changePassword');
+    }
+
+    const deleteNav = () => {
+        navigate('/deleteAccount');
+    }
     return (
         <div className="profile">
-            <p>{localStorage.getItem("username")}</p>
+            <p>User: {username}</p>
+            <p>Role: {accountInfo.role}</p>
+            <button onClick={logout}>Log Out</button>
+            <button onClick={userNav}>Change Username</button>
+            <button onClick={passNav}>Change Password</button>
+            <button onClick={deleteNav}>Delete Account</button>
         </div>
     );
 }
 
-export default profile;
+export default Profile;
